b5eafbd9b1c6f1d59b9d3fc9412a910669f29906
dvill <villalbautria08@gmail.com>
2025-07-26 12:39:04 -0500

    feat: improved bottom bar

--- src/routes/(app)/bottom-navbar.svelte
+++ src/routes/(app)/bottom-navbar.svelte
@@ -2,34 +2,45 @@
   import TablerBuildingBank from "~icons/tabler/building-bank";
   import TablerPlus from "~icons/tabler/plus";
   import TablerMenuDeep from "~icons/tabler/menu-deep";
-  import TablerClipboardListFilled from "~icons/tabler/clipboard-list-filled";
+  import TablerChartDonutFilled from "~icons/tabler/chart-donut-filled";
   import TablerPigFilled from "~icons/tabler/pig-filled";
-  import { Button, buttonVariants } from "$lib/components/ui/button";
-
-  const itemStyles = buttonVariants({
-    variant: "ghost",
-    class: "flex-col gap-1 h-auto",
-  });
 </script>
 
-<nav class="flex gap-4 justify-center items-center bg-primary/5 p-2">
-  <a class={itemStyles} href="/home">
-    <TablerBuildingBank />
-    <p>Home</p>
-  </a>
-  <a class={itemStyles} href="/records">
-    <TablerMenuDeep />
-    <p>Records</p>
-  </a>
-  <Button size='icon' class='rounded-full'>
-    <TablerPlus />
-  </Button>
-  <a class={itemStyles} href="/budgets">
-    <TablerPigFilled />
-    <p>Budget</p>
-  </a>
-  <a class={itemStyles} href="/menu">
-    <TablerClipboardListFilled />
-    <p>Menu</p>
-  </a>
-</nav>
+<div id="bottom-bar" class="root">
+  <div class="content">
+    <a href="/home" class="item group">
+      <TablerBuildingBank />
+    </a>
+    <a href="/budget" class="item group">
+      <TablerPigFilled />
+    </a>
+    <button class="item group">
+      <TablerPlus />
+    </button>
+    <a href="/charts" class="item group">
+      <TablerChartDonutFilled />
+    </a>
+    <a href="/menu" class="item group">
+      <TablerMenuDeep />
+    </a>
+  </div>
+</div>
+
+<style lang="postcss">
+  @reference '../../app.css';
+  .root {
+    @apply w-full bg-white;
+    @apply dark:bg-primary/5;
+  }
+  .content {
+    @apply grid h-full max-w-lg grid-cols-5 mx-auto;
+  }
+  .item {
+    @apply inline-flex flex-col items-center justify-center p-4 hover:bg-gray-50;
+    @apply dark:hover:bg-gray-800;
+  }
+  :global(#bottom-bar .item svg) {
+    @apply size-6 text-gray-500 group-hover:text-primary;
+    @apply dark:text-gray-400 dark:group-hover:text-primary;
+  }
+</style>
